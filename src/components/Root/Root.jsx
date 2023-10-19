9e32acbbf6319ec33fc90b1873442573576b44fd
farhanshahriyar <farhan.abir9999@gmail.com>
2023-10-19 14:27:29 +0600

    added homepage

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,6 +4,7 @@ import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 
 
+
 const Root = () => {
   return (
     <div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Section1 from '../../layouts/Section1'
+import Section2 from '../../layouts/Section2'
+import Section3 from '../../layouts/Section3'
+import Section4 from '../../layouts/Section4'
+import Section5 from '../../layouts/Section5'
+import Clients from '../../layouts/Clients'
+import NewsLetter from '../../components/NewsLetter/NewsLetter'
 
 const Home = () => {
   return (
     <div>
-       This is Home
+       <Section1/>
+       <Section2/>
+       <Section3/>
+       <Section4/>
+       <Section5/>
+       <Clients/>
+       <NewsLetter/>
     </div>
   )
 }
